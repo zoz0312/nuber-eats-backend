0d786613a84a4e8ebad0d4c9cb20227136f10bf8
zoz0312 <zoz0312@naver.com>
2020-11-19 16:04:32 +0900

    Guard bug fix

--- src/auth/role.decorator.ts
+++ src/auth/role.decorator.ts
@@ -3,4 +3,4 @@ import { UserRole } from "src/users/entities/user.entity";
 
 export type AllowedRoles = keyof typeof UserRole | 'Any';
 
-export const Role = (roles: AllowedRoles[]) => SetMetadata('role', roles);
+export const Role = (roles: AllowedRoles[]) => SetMetadata('roles', roles);
